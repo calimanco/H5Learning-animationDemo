f7d489cf84d7511b2a34d8a296e34d38e698144f
zwei76 <ericlin8840461@hotmail.com>
2018-03-03 00:00:41 +0800

    update to es6

--- common/utils.js
+++ common/utils.js
@@ -1,10 +1,10 @@
-let utils = {};
-utils.captureMouse = function(element) {
-  let mouse = {
+const utils = {};
+utils.captureMouse = function captureMouse(element) {
+  const mouse = {
     x: 0,
     y: 0,
   };
-  element.addEventListener('mousemove', function(event) {
+  element.addEventListener('mousemove', (event) => {
     let x;
     let y;
     if (event.pageX || event.pageY) {
@@ -24,27 +24,27 @@ utils.captureMouse = function(element) {
   return mouse;
 };
 
-utils.captureTouch = function(element) {
-  let touch = {
+utils.captureTouch = function captureTouch(element) {
+  const touch = {
     x: null,
     y: null,
     isPressed: false,
   };
 
-  element.addEventListener('touchstart', function(event) {
+  element.addEventListener('touchstart', () => {
     touch.isPressed = true;
   }, false);
 
-  element.addEventListener('touchend', function(event) {
+  element.addEventListener('touchend', () => {
     touch.isPressed = false;
     touch.x = null;
     touch.y = null;
   }, false);
 
-  element.addEventListener('touchmove', function(event) {
+  element.addEventListener('touchmove', (event) => {
     let x;
     let y;
-    let touchEvent = event.touches[0]; // 单指触控
+    const touchEvent = event.touches[0]; // 单指触控
 
     if (touchEvent.pageX || touchEvent.pageY) {
       x = touchEvent.pageX;
@@ -63,44 +63,44 @@ utils.captureTouch = function(element) {
   return touch;
 };
 
-utils.colorToRGB = function(color, alpha) {
+utils.colorToRGB = function colorToRGB(_color, _alpha) {
+  let color = _color;
   // 如果是字符串，转换成数字
   if (typeof color === 'string' && color[0] === '#') {
     color = parseInt(color.slice(1), 16);
   }
-  alpha = (alpha === undefined) ? 1:alpha;
+  const alpha = (_alpha === undefined) ? 1 : _alpha;
 
   // 提取各颜色的值
-  let r = color >> 16 & 0xFF;
-  let g = color >> 8 & 0xFF;
-  let b = color & 0xFF;
+  const r = color >> 16 & 0xFF;
+  const g = color >> 8 & 0xFF;
+  const b = color & 0xFF;
   // 检查透明度，并剔除非法值
-  let a = (alpha < 0) ? 0 : ((alpha > 1) ? 1:alpha);
+  const a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
 
   // 判断是否以rgba表示
   if (a === 1) {
-    return 'rgb('+r+','+g+','+b+')';
-  } else {
-    return 'rgba('+r+','+g+','+b+','+a+')';
+    return `rgb(${r},${g},${b})`;
   }
+  return `rgba(${r},${g},${b},${a})`;
 };
 
-utils.parseColor = function(color, toNumber) {
+utils.parseColor = function parseColor(_color, toNumber) {
+  let color = _color;
   if (toNumber === true) {
     if (typeof color === 'number') {
       // 判断是数字，剔除小数即可
-      return (color|0);
+      return (color | 0);
     }
     if (typeof color === 'string' && color[0] === '#') {
       color = color.slice(1);
     }
     return parseInt(color, 16);
-  } else {
-    if (typeof color === 'number') {
-      // 确保十六进制数字被填了
-      // 先去掉小数点，然后转16进制，补足6位，剪切出后6位
-      color = '#' + ('00000' + (color | 0).toString(16)).substr(-6);
-    }
-    return color;
   }
+  if (typeof color === 'number') {
+    // 确保十六进制数字被填了
+    // 先去掉小数点，然后转16进制，补足6位，剪切出后6位
+    color = `#${(`00000${(color | 0).toString(16)}`).substr(-6)}`;
+  }
+  return color;
 };
