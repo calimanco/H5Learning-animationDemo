873241958a38994a58fc20c7f41d825e01a064a4
zwei76 <ericlin8840461@hotmail.com>
2018-03-11 15:58:59 +0800

    add part7

--- common/ball.js
+++ common/ball.js
@@ -13,6 +13,8 @@ class Ball {
   constructor(radius = 40, color = '#ff0000') {
     this.x = 0;
     this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
     this.radius = radius;
     this.rotation = 0;
     this.scaleX = 1;
