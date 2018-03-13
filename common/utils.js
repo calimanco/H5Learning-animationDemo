84a4c40f481b5073b345e8b855235bf6cd58d73c
zwei76 <ericlin8840461@hotmail.com>
2018-03-14 00:00:47 +0800

    add intersects

--- common/utils.js
+++ common/utils.js
@@ -148,3 +148,10 @@ utils.parseColor = function parseColor(_color, toNumber) {
 utils.containsPoint = function containsPoint(rect, x, y) {
   return !(x < rect.x || x > rect.x + rect.width || y < rect.y || y > rect.y + rect.height);
 };
+
+utils.intersects = function intersects(rectA, rectB) {
+  return !(rectA.x + rectA.width < rectB.x ||
+    rectB.x + rectB.width < rectA.x ||
+    rectA.y + rectA.height < rectB.y ||
+    rectB.y + rectB.height < rectA.y);
+};
