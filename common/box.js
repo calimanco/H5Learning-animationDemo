45d53358fcd452aad30385f1de9b4baa80bb358d
zwei76 <ericlin8840461@hotmail.com>
2018-03-14 17:24:18 +0800

    add part8

---
+++ common/box.js
@@ -0,0 +1,39 @@
+/**
+  * 盒子类
+  * @class Representing a box.
+  */
+/* global utils */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "Box" }] */
+class Box {
+  constructor(width = 50, height = 50, color = '#ff0000') {
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.vx = 0;
+    this.vy = 0;
+    this.rotation = 0;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.color = utils.parseColor(color);
+    this.lineWidth = 1;
+  }
+
+  draw(_context) {
+    const context = _context;
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.scale(this.scaleX, this.scaleY);
+    context.lineWidth = this.lineWidth;
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.rect(0, 0, this.width, this.height);
+    context.closePath();
+    context.fill();
+    if (this.lineWidth > 0) {
+      context.stroke();
+    }
+    context.restore();
+  }
+}
